ffe28bdfff094a342d22bdcd474e0d900c197d14
Vikas Patoli <vksonpdl@gmail.com>
2021-12-10 08:57:16 +0530

    update

--- src/main/java/com/example/demo/util/DecryptionUtil.java
+++ src/main/java/com/example/demo/util/DecryptionUtil.java
@@ -1,17 +1,12 @@
 package com.example.demo.util;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
-import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
@@ -28,8 +23,9 @@ import com.google.protobuf.ByteString;
 @Component
 public class DecryptionUtil {
 
-	@Value("${key.private}")
-	String privateKeyString;
+	/*
+	 * @Value("${key.private}") String privateKeyString;
+	 */
 
 	@Value("${gcp.project-id}")
 	String projectId;
@@ -46,32 +42,36 @@ public class DecryptionUtil {
 	@Value("${gcp.key-version-rsa}")
 	String keyVersion;
 
-	public String decrypt(String encryptedString) throws IllegalBlockSizeException, BadPaddingException,
-			NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
-
-		if (StringUtils.hasText(encryptedString)) {
-
-			encryptedString = encryptedString.replaceAll(AppConstants.CHAR_ASTERISK, AppConstants.CHAR_FORWARDSLASH);
-
-			byte[] privateKeyBytes = privateKeyString.getBytes(StandardCharsets.UTF_8);
-			privateKeyBytes = Base64.getDecoder().decode(privateKeyBytes);
-
-			KeyFactory keyFactory2 = KeyFactory.getInstance(AppConstants.ENCRYPTION_MODE_RSA);
-			PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-
-			RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory2.generatePrivate(privateKeySpec);
-
-			Cipher decryptCipher = Cipher.getInstance(AppConstants.ENCRYPTION_MODE_RSA);
-			decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
-			byte[] decryptedMessageBytes = decryptCipher.doFinal(Base64.getDecoder().decode(encryptedString));
-			String decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
-
-			return decryptedMessage;
-
-		} else {
-			return encryptedString;
-		}
-	}
+	/*
+	 * public String decrypt(String encryptedString) throws
+	 * IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException,
+	 * InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException {
+	 * 
+	 * if (StringUtils.hasText(encryptedString)) {
+	 * 
+	 * encryptedString = encryptedString.replaceAll(AppConstants.CHAR_ASTERISK,
+	 * AppConstants.CHAR_FORWARDSLASH);
+	 * 
+	 * byte[] privateKeyBytes = privateKeyString.getBytes(StandardCharsets.UTF_8);
+	 * privateKeyBytes = Base64.getDecoder().decode(privateKeyBytes);
+	 * 
+	 * KeyFactory keyFactory2 =
+	 * KeyFactory.getInstance(AppConstants.ENCRYPTION_MODE_RSA); PKCS8EncodedKeySpec
+	 * privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+	 * 
+	 * RSAPrivateKey privateKey = (RSAPrivateKey)
+	 * keyFactory2.generatePrivate(privateKeySpec);
+	 * 
+	 * Cipher decryptCipher = Cipher.getInstance(AppConstants.ENCRYPTION_MODE_RSA);
+	 * decryptCipher.init(Cipher.DECRYPT_MODE, privateKey); byte[]
+	 * decryptedMessageBytes =
+	 * decryptCipher.doFinal(Base64.getDecoder().decode(encryptedString)); String
+	 * decryptedMessage = new String(decryptedMessageBytes, StandardCharsets.UTF_8);
+	 * 
+	 * return decryptedMessage;
+	 * 
+	 * } else { return encryptedString; } }
+	 */
 
 	public String decryptFromCloud(String encryptedString) throws IllegalBlockSizeException, BadPaddingException,
 			NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException,
